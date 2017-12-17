7623343391455d18d9b0fc577fb7b998ecc719f6
pukiscuy <pukiscuy@gmail.com>
2017-12-17 15:59:55 +0700

    template dashboard

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -3,43 +3,30 @@
 
   <div class="px-content">
     <div class="row">
-      <div class="col-md-7 fadeIn animated">   
+      <div class="col-md-12 fadeIn animated">   
         <div class="panel panel-info panel-dark">
           <div class="panel-heading">
-            <span class="panel-title"><i class="panel-title-icon fa fa-bar-chart"></i>{{ trinata::titleActionForm() }}</span>
+            <span class="panel-title"><i class="panel-title-icon fa fa-home"></i>{{ trinata::titleActionForm() }}</span>
           </div>   
             <div class="panel-body">
               <div class="row">
                 <div class="col-md-12">
-                  <canvas id="chart-bars" width="400" height="250"></canvas>
+                  <img src="{{ asset(null) }}backend/assets/images/dashboard.png" width="100%">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12"><br/>
+                  <span class="font-size-24">Welcome to Inventory System PGN</span>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra tristique nunc vitae bibendum. Vestibulum vitae nunc sed nisi efficitur dignissim. Nulla hendrerit nisl nec lectus viverra tincidunt. 
+                  </p>
                 </div>
               </div>
             </div>
         </div>
       </div>
 
-      <div class="col-md-5 fadeIn animated"> 
-        <div class="panel panel-info panel-dark">
-          <div class="panel-heading">
-            <span class="panel-title"><i class="panel-title-icon fa fa-list"></i>User Activities</span>
-          </div> 
-          <div class="row p-a-3">
-            <table class = 'table table-bordered'>
-                    <tbody>
-                        @foreach($last as $row)
-                            
-                            <tr class = "{{ $row->id % 2 == 0 ? 'success' : 'danger' }}">
-                                <td>{{ $row->action }}</td>
-                                <td>{{ Carbon\Carbon::parse($row->created_at)->format("d F ,Y H:i:s") }}</td>
-                            </tr>
-
-                        @endforeach
-                    </tbody>
-                </table>
-
-          </div>
-        </div>
-      </div>
+      
     </div>
 
   </div>
@@ -49,23 +36,6 @@
 @push('script-js')
 <script type="text/javascript">
   
-    $(function() {
-      var data = {
-        labels: ["{!! $charts['dates'] !!}"],
-        datasets: [{
-          label:           'User Activities',
-          data:            [{{$charts['data']}}],
-          borderWidth:     1,
-          backgroundColor: pxUtil.hexToRgba('#1076BC', 0.3),
-          borderColor:     '#1076BC',
-        }],
-      };
-
-      new Chart(document.getElementById('chart-bars').getContext("2d"), {
-        type: 'bar',
-        data: data,
-      });
-    });
 
 </script>
 @endpush
