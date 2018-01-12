57e16e7cad155edf199eda36cc8106fdb0cb68e2
ruee\user <arumtri.iswari@gmail.com>
2018-01-12 18:20:42 +0700

    login

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -7,8 +7,8 @@
 
 
   <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin" rel="stylesheet" type="text/css">
-  <link href="{{ asset(null) }}backend/assets/css/ionicons.min.css" rel="stylesheet" type="text/css">
-  <link href="{{ asset(null) }}backend/assets/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
   <link href="{{ asset(null) }}backend/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" class="px-demo-stylesheet-bs">
   <link href="{{ asset(null) }}backend/assets/css/admin.min.css" rel="stylesheet" type="text/css" class="px-demo-stylesheet-core">
--- resources/views/backend/layouts/login.blade.php
+++ resources/views/backend/layouts/login.blade.php
@@ -8,8 +8,8 @@
   <title>PGN CPanel</title>
 
   <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300&subset=latin" rel="stylesheet" type="text/css">
-  <link href="{{ asset(null) }}backend/assets/css/ionicons.min.css" rel="stylesheet" type="text/css">
-  <link href="{{ asset(null) }}backend/assets/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
   <link href="{{ asset(null) }}backend/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" class="px-demo-stylesheet-bs">
   <link href="{{ asset(null) }}backend/assets/css/admin.min.css" rel="stylesheet" type="text/css" class="px-demo-stylesheet-core">
@@ -23,7 +23,9 @@
   <link href="{{ asset(null) }}backend/assets/css/style.css" rel="stylesheet" type="text/css">
   <link href="{{ asset(null) }}backend/assets/css/animate.css" rel="stylesheet" type="text/css">
 
-  <script type="text/javascript" src="{{ asset(null) }}backend/assets/js/holder.js"></script>
+  <!-- <script type="text/javascript" src="{{ asset(null) }}backend/assets/js/holder.js"></script> -->
+  
+  <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/holder/2.9.0/holder.js"></script>
 
   <!-- Pace.js -->
   <script src="{{ asset(null) }}backend/assets/pace/pace.min.js"></script>
