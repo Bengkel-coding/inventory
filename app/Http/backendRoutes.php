259c45194d738b363fc261940e7daaadad818a08
ovancop <ovan89@gmail.com>
2018-01-14 16:17:25 +0700

    admin route replace

--- app/Http/backendRoutes.php
+++ app/Http/backendRoutes.php
@@ -7,7 +7,8 @@
 			foreach(injectModel('Menu')->where('controller','!=','#')->get() as $row)
 			{	
 				$path = app_path('Http/Controllers/Backend/'.$row->controller.'.php');
-
+				$path = str_replace('\\', '/', $path);
+				
 				if(file_exists($path))
 				{
 					Route::controller($row->slug,'Backend\\'.$row->controller);
