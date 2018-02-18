10261fb3b26c5707e878fcd6567fca7c676e8562
ovancop <ovan89@gmail.com>
2018-02-18 19:52:07 +0700

    log mutasi mro

--- app/Http/Controllers/Backend/Mutasi/MutasiMroController.php
+++ app/Http/Controllers/Backend/Mutasi/MutasiMroController.php
@@ -127,7 +127,7 @@ class MutasiMroController extends TrinataController
                 $mutation->status = 1;
                 
                 if($mutation->save()){
-                    $log_mutation = new \App\Models\logMutation; //log
+                    $log_mutation = new \App\Models\LogMutation; //log
                     $log_mutation->material_id = $model->id;
                     $log_mutation->amount = $model->amount;
                     $log_mutation->proposed_amount = $request->proposed_amount;
