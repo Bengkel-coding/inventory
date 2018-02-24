6f1fc9c2a54f2493b1761f20ff24452cd386344d
ovancop <ovan89@gmail.com>
2018-02-24 18:30:20 +0700

    laporan

--- app/Http/Controllers/Backend/LaporanController.php
+++ app/Http/Controllers/Backend/LaporanController.php
@@ -68,6 +68,8 @@ class LaporanController extends TrinataController
     	$categoryList = ['mro'=>'sheetMro',
                         'mroabt'=>'sheetMroAbt', 
                         'investasi'=>'sheetInvestasi',
+                        'eksjar'=>'sheetEksjar',
+                        'tercatat'=>'sheetTercatat',
                         ];
     	
     	$this->prepareExcel($categoryList, $request);
@@ -110,7 +112,9 @@ class LaporanController extends TrinataController
 
     public function prepareData($value, $key)
     {
-    	$needCode = ['mroabt','investasi'];
+        $needCode = ['mroabt','investasi','eksjar','tercatat'];
+    	$parseData = ['mro'=>'dataMro','mroabt'=>'dataMroabt','investasi'=>'dataInvestasi',
+                    'eksjar'=>'dataEksjar','tercatat'=>'dataTercatat'];
 
     	$datas = [];
 		$datas[] = $key+1;
@@ -118,14 +122,157 @@ class LaporanController extends TrinataController
 		$datas[] = $value->komag;
 		if(in_array($value->type, $needCode)) $datas[] = $value->kode;
 		$datas[] = $value->description;
-		$datas[] = ucfirst($value->unit);
-		$datas[] = $value->year_acquisition;
-		$datas[] = $value->amount;
-		$datas[] = number_format($value->unit_price);
-		$datas[] = number_format($value->amount * $value->unit_price);
+
+		/*if ($value->type == 'eksjar') {
+            $datas = array_merge($datas, $this->dataEksjar($value));
+        } elseif ($value->type == 'tercatat') {
+            $datas = array_merge($datas, $this->dataTercatat($value));
+        } */
+
+        $datas = array_merge($datas, $this->{$parseData[$value->type]}($value));
 
 		return $datas;
     }
+
+    public function dataMro($value)
+    {
+        $data = [];
+        
+        $data[] = ucfirst($value->unit);
+        $data[] = $value->year_acquisition;
+        $data[] = $value->amount;
+        $data[] = number_format($value->unit_price);
+        $data[] = number_format($value->amount * $value->unit_price);  
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = $this->getWarehouse($value->warehouse_id);
+    
+        return $data;
+    }
+
+    public function dataMroabt($value)
+    {
+        $data = [];
+        
+        $data[] = ucfirst($value->unit);
+        $data[] = $value->year_acquisition;
+        $data[] = $value->amount;
+        $data[] = number_format($value->unit_price);
+        $data[] = number_format($value->amount * $value->unit_price);  
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = $this->getWarehouse($value->warehouse_id);
+    
+        return $data;
+    }
+
+    public function dataInvestasi($value)
+    {
+        $data = [];
+        
+        $data[] = ucfirst($value->unit);
+        $data[] = $value->year_acquisition;
+        $data[] = $value->amount;
+        $data[] = number_format($value->unit_price);
+        $data[] = number_format($value->amount * $value->unit_price);  
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = '';
+        $data[] = $this->getWarehouse($value->warehouse_id);
+    
+        return $data;
+    }
+
+    public function dataEksjar($value)
+    {
+        $data = [];
+        $model = \App\Models\MaterialEksjar::whereMaterialId($value->id)->first();
+        if ($model) {
+            $data[] = $model->merk;
+            $data[] = $model->specification;
+            $data[] = $value->serialnumber;
+            $data[] = $model->year_production;
+            $data[] = (int) $value->amount;
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = $this->getWarehouse($value->warehouse_id);
+        }
+        return $data;
+    }
+
+    public function dataTercatat($value)
+    {
+        $data = [];
+        // $model = \App\Models\MaterialEksjar::whereMaterialId($value->id)->first();
+        
+            $data[] = $value->unit;
+            $data[] = $value->year_acquisition;
+            $data[] = (int) $value->amount;
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = '';
+            $data[] = $this->getWarehouse($value->warehouse_id);
+        
+        return $data;
+    }
+
+    public function getWarehouse($id)
+    {
+        return \App\Models\Warehouse::whereId($id)->first()->name;
+    }
+
     public function sheetMro($sheet)
     {
     	$header = ['Deskripsi Material (berdasarkan Master KOMAG)',
@@ -222,7 +369,7 @@ class LaporanController extends TrinataController
                         'Harga Total','Penerimaan','','Pengeluaran','','Jumlah Material','Harga Satuan',
                         'Harga Total','','','',''
                         ];
-        $subSubHeader = ['','','','','','','','','','','','','','','','','',
+        $subSubHeader = ['','','','','','','','','','','','','','','','',
                             'Jumlah Material','Harga Total',
                             'Jumlah Material','Harga Total',
                             '','','','','','',''];
@@ -255,7 +402,80 @@ class LaporanController extends TrinataController
 
     public function sheetEksjar($sheet)
     {
+        $header = ['Kode MRO-ABT/ Kode MI','Deskripsi Material (berdasarkan Master KOMAG)',
+                        'Merk','Spesifikasi','Serial Number','Tahun Pembuatan / Perolehan',
+                        'Saldo Awal','Penerimaan','Pengeluaran','Pengembalian / Return',
+                        'Mutasi Antar Gudang','','Saldo Akhir',
+                        'Lokasi Awal','Keterangan','Lokasi Penyimpanan'
+                        ];
+        $subHeader = ['','','','','','','','','','Jumlah Material',
+                        'Jumlah Material','Jumlah Material','Jumlah Material','Penerimaan','Pengeluaran',
+                        'Jumlah Material','','',''
+                        ];
+        $subSubHeader = ['','','','','','','','','','','','','','Jumlah Material','Jumlah Material','','',
+                        '',''];
 
+        $sheet->setBorder('A1:S1', 'thin');
+        $sheet->setBorder('A2:S2', 'thin');
+        $sheet->setBorder('A3:S3', 'thin');
+
+        // dd(array_merge($this->header, $header), $subHeader);
+        $sheet->row(1, array_merge($this->header, $header)); 
+        $sheet->row(2, $subHeader); 
+        $sheet->row(3, $subSubHeader); 
+
+        // merge column
+        $setMerge = ['N1:O1','A1:A3','B1:B3',
+                    'C1:C3','D1:D3','E1:E3','F1:F3','G1:G3','H1:H3','I1:I3','J2:J3','K2:K3',
+                    'L2:L3','M2:M3','P2:P3','Q1:Q3','R1:R3','S1:S3'];
+        $this->setMergeCell($sheet, $setMerge);
+
+        $setWidth = ['A'=>5,'B'=>20,'C'=>20,'D'=>20,'E'=>10,'F'=>20];
+        $this->setWidth($sheet, $setWidth);
+
+        $setAlignment = ['A1:A3'=>'#7FCC4C', 'B1:B3'=>'#7FCC4C','C1:C3'=>'#7FCC4C','D1:D3'=>'#7FCC4C',
+                        'E1:E3'=>'#7FCC4C','F1:F3'=>'#7FCC4C','G1:G3'=>'#7FCC4C','H1:H3'=>'#7FCC4C',
+                        'I1:I3'=>'#7FCC4C','W1:W3'=>'#7FCC4C','X1:X3'=>'#7FCC4C','Y1:Y3'=>'#7FCC4C',
+                        'Z1:Z3'=>'#7FCC4C','AA1:AA3'=>'#7FCC4C','AB1:AB3'=>'#7FCC4C'];
+    }
+
+    public function sheetTercatat($sheet)
+    {
+        $header = ['Kode MI','Deskripsi Material (berdasarkan Master KOMAG)',
+                        'Satuan','Tahun Perolehan',
+                        'Saldo Awal','Penerimaan','Pengeluaran','Pengembalian / Return',
+                        'Mutasi Antar Gudang','','Saldo Akhir',
+                        'Keterangan','Lokasi'
+                        ];
+        $subHeader = ['','','','','','','','Jumlah Material',
+                        'Jumlah Material','Jumlah Material','Jumlah Material','Penerimaan','Pengeluaran',
+                        'Jumlah Material','',''
+                        ];
+        $subSubHeader = ['','','','','','','','','','','','Jumlah Material','Jumlah Material','','',
+                        ''];
+
+        $sheet->setBorder('A1:P1', 'thin');
+        $sheet->setBorder('A2:P2', 'thin');
+        $sheet->setBorder('A3:P3', 'thin');
+
+        // dd(array_merge($this->header, $header), $subHeader);
+        $sheet->row(1, array_merge($this->header, $header)); 
+        $sheet->row(2, $subHeader); 
+        $sheet->row(3, $subSubHeader); 
+
+        // merge column
+        $setMerge = ['L1:M1','A1:A3','B1:B3',
+                    'C1:C3','D1:D3','E1:E3','F1:F3','G1:G3','H2:H3','I2:I3','J2:J3','K2:K3',
+                    'N2:N3','O1:O3','P1:P3'];
+        $this->setMergeCell($sheet, $setMerge);
+
+        $setWidth = ['A'=>5,'B'=>20,'C'=>20,'D'=>20,'E'=>10,'F'=>20];
+        $this->setWidth($sheet, $setWidth);
+
+        $setAlignment = ['A1:A3'=>'#7FCC4C', 'B1:B3'=>'#7FCC4C','C1:C3'=>'#7FCC4C','D1:D3'=>'#7FCC4C',
+                        'E1:E3'=>'#7FCC4C','F1:F3'=>'#7FCC4C','G1:G3'=>'#7FCC4C','H1:H3'=>'#7FCC4C',
+                        'I1:I3'=>'#7FCC4C','W1:W3'=>'#7FCC4C','X1:X3'=>'#7FCC4C','Y1:Y3'=>'#7FCC4C',
+                        'Z1:Z3'=>'#7FCC4C','AA1:AA3'=>'#7FCC4C','AB1:AB3'=>'#7FCC4C'];
     }
 
     public function setMergeCell($sheet, $data)
